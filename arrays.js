33d13ff2abc97224515900ec1be129e71e92f9da
Kayla Hansen <shiverr@gmail.com>
2017-09-05 00:49:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ var chocolateBars = [
   'skittles'
 ]
 
-function addElementToBeginningOfArray([chocolateBars], 'element') {
+function addElementToBeginningOfArray(chocolateBars, 'element') {
   chocolateBars.push('element')
    return chocolateBars
  }
