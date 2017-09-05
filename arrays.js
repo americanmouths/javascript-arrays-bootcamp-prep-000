b68ca954607ad47c8c1c712d9a5f245ff6bea6cb
Kayla Hansen <shiverr@gmail.com>
2017-09-05 01:15:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,7 +29,7 @@ function accessElementInArray(array, index) {
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
-  array
+  return array
 }
 
 function removeElementFromBeginningOfArray(array) {
